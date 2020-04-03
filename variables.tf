7230fb0e7453d4981ada763dc5bec1bfef3c2911
Nick Pellant <nick@nickpellant.com>
2020-04-03 18:28:02 +0100

    Perform terraform 0.12 upgrade

--- variables.tf
+++ variables.tf
@@ -1,29 +1,29 @@
 variable "namespace" {
-  type        = "string"
+  type        = string
   description = "Namespace (e.g. `eg` or `cp`)"
   default     = ""
 }
 
 variable "stage" {
-  type        = "string"
+  type        = string
   description = "Stage (e.g. `prod`, `dev`, `staging`, `infra`)"
   default     = ""
 }
 
 variable "delimiter" {
-  type        = "string"
+  type        = string
   default     = "-"
   description = "Delimiter to be used between `namespace`, `stage`, `name`, and `attributes`"
 }
 
 variable "attributes" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "Additional attributes (e.g. `policy` or `role`)"
 }
 
 variable "tags" {
-  type        = "map"
+  type        = map(string)
   default     = {}
   description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)"
 }
@@ -49,7 +49,7 @@ variable "path" {
 
 variable "groups" {
   description = "List of IAM user groups this user should belong to in the account"
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
@@ -76,3 +76,4 @@ variable "password_length" {
   description = "The length of the generated password"
   default     = 24
 }
+
